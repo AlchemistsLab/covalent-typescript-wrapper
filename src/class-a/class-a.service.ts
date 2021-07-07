b05436675966abba30724e2ee76729bca6117ff0
Nick <nithee@harmonyx.co>
2021-07-08 00:28:27 +0700

    complete class a api warpper

--- src/class-a/class-a.service.ts
+++ src/class-a/class-a.service.ts
@@ -11,10 +11,7 @@ export class ClassAService {
     this.key = this.configService.get("COVALENT_HQ_API_KEY")
   }
   // GET {chain_id}/address/{address}/balances_v2/
-  public async getTokenBalanceForAddress(
-    args: { chainId, address },
-    params
-  ) {
+  public async getTokenBalanceForAddress(args: { chainId, address }, params) {
     try {
       let parameters = {
         "nft": params.nft ? params.nft : false,
@@ -32,7 +29,6 @@ export class ClassAService {
       const response = await this.httpService
         .get(requestUrl, { params: { ...parameters } })
         .toPromise()
-      console.log("resp", response.request)
       return get(response, ["data", "data"], {})
     } catch (e) {
       throw e
@@ -40,51 +36,358 @@ export class ClassAService {
   }
 
   // GET {chain_id}/address/{address}/portfolio_v2/
-  public async getHistoricalPortfolioValueOverTime() {
+  public async getHistoricalPortfolioValueOverTime({ chainId, address }, params) {
+    try {
+      let parameters = {
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/address/${address}/portfolio_v2/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
   }
 
   // GET {chain_id}/address/{address}/transactions_v2/
-  public async getTransactions() { }
+  public async getTransactions({ chainId, address }, params) {
+    try {
+      let parameters = {
+        "block-signed-at-asc": params.blockSignedAtAsc,
+        "no-logs": params.noLogs,
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/address/${address}/transactions_v2/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/address/{address}/transfers_v2/
-  public async getERC20Transfers() { }
+  public async getERC20Transfers({ chainId, address }, params) {
+    try {
+      let parameters = {
+        "contract-address": params.contractAddress,
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/address/${address}/transfers_v2/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/block_v2/{block_height}/
-  public async getBlock() { }
+  public async getBlock({ chainId, blockHeight }, params) {
+    try {
+      let parameters = {
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/block_v2/${blockHeight}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/block_v2/{start_date}/{end_date}/
-  public async getBlockHeight() { }
+  public async getBlockHeight({ chainId, startDate, endDate }, params) {
+    try {
+      let parameters = {
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/block_v2/${startDate}/${endDate}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/events/address/{address}/
-  public async getLogEventsByContractAddress() { }
+  public async getLogEventsByContractAddress({ chainId, address }, params) {
+    try {
+      let parameters = {
+        "starting-block": params.startingBlock,
+        "ending-block": params.endingBlock,
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/events/address/${address}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/events/topics/{topic}/
-  public async getLogEventsByTopicHashes() { }
+  public async getLogEventsByTopicHashes({ chainId, topic }, params) {
+    try {
+      let parameters = {
+        "secondary-topics": params.secondaryTopics,
+        "starting-block": params.startingBlock,
+        "ending-block": params.endingBlock,
+        "sender-address": params.senderAddress,
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/events/topics/${topic}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
+
+  public async getChangesInTokenholdersBetweenTwoBlockHeights({ chainId, address }, params) {
+    try {
+      let parameters = {
+        "starting-block": params.secondaryTopics,
+        "ending-block": params.endingBlock,
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/tokens/${address}/token_holders_changes/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/tokens/{address}/nft_metadata/{token_id}/
-  public async getExternalNFTMetadata() { }
+  public async getExternalNFTMetadata({ chainId, contractAddress, tokenId }, params) {
+    try {
+      let parameters = {
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/tokens/${contractAddress}/nft_metadata/${tokenId}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/tokens/{address}/nft_token_ids/
-  public async getNFTTokensIDs() { }
+  public async getNFTTokensIDs({ chainId, contractAddress }, params) {
+    try {
+      let parameters = {
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/tokens/${contractAddress}/nft_token_ids/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/tokens/{address}/nft_transactions/{token_id}/
-  public async getNFTTransactions() { }
-
-  // GET {chain_id}/tokens/{address}/token_holders_changes/
-  public async getChangesInTokenHolders() { }
-
-  // GET {chain_id}/tokens/{address}/token_holders/
-  public async getTokenHoldersASOfBlockHeight() { }
+  public async getNFTTransactions({ chainId, contractAddress, tokenId }, params) {
+    try {
+      let parameters = {
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/tokens/${contractAddress}/nft_transactions/${tokenId}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/tokens/tokenlists/{id}/
-  public async getAllContractMetaData() { }
+  public async getAllContractMetaData({ chainId, id }, params) {
+    try {
+      let parameters = {
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/tokens/tokenlists/${id}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET {chain_id}/transaction_v2/{tx_hash}/
-  public async getTransaction() { }
+  public async getTransaction({ chainId, txHash }, params) {
+    try {
+      let parameters = {
+        "no-logs": params.noLogs,
+        "page-number": params.pageNumber,
+        "page-size": params.pageSize,
+        "quote-currency": params.quoteCurrency,
+        "format": params.format,
+        "primer": params.primer,
+        "match": params.match,
+        "group": params.group,
+        "sort": params.sort,
+        "skip": params.skip,
+        "limit": params.limit
+      }
+      const requestUrl = `/v1/${chainId}/transaction_v2/${txHash}/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl, { params: { ...parameters } })
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET chains/
-  public async getAllChain() { }
+  public async getAllChain() {
+    try {
+      const requestUrl = `/v1/chains/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl)
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 
   // GET chains/status/
-  public async getAllChainStatuses() { }
+  public async getAllChainStatuses() {
+    try {
+      const requestUrl = `/v1/chains/status/?key=${this.key}`
+      const response = await this.httpService
+        .get(requestUrl)
+        .toPromise()
+      return get(response, ["data", "data"], {})
+    } catch (e) {
+      throw e
+    }
+  }
 }
