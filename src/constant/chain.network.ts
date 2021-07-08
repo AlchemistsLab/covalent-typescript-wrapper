31fd1e216b4182bfb1342d121ceea2601fedc429
Nick <nithee@harmonyx.co>
2021-07-08 23:48:41 +0700

    add network enums

---
+++ src/constant/chain.network.ts
@@ -0,0 +1,16 @@
+export enum ChainNetwork {
+  Ethereum = "1",
+  PolygonMatic = "137",
+  AvalancheCChain = "43114",
+  BSC = "56",
+  FantomOpera = "250"
+}
+
+export enum ChainNetworkTestnets {
+  PolygonMaticMumbai = "80001",
+  FujiCChain = "43113",
+  Kovan = "42",
+  BSC = "97",
+  MoonbaseAlpha = "1287",
+  Fantom = "4002"
+}
